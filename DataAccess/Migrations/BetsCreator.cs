809cbc381a2600ceeed65888a61bd72b7bdb4de0
Robert Adam <robert.adam@uipath.com>
2024-07-16 18:03:52 +0300

    Modify tables

--- DataAccess/Migrations/BetsCreator.cs
+++ DataAccess/Migrations/BetsCreator.cs
@@ -5,22 +5,19 @@ namespace DataAccess.Migrations
     [Migration(202407156)]
     public class Migration_202407156_Add_Bets : Migration
     {
+        public override void Up()
+        {
+            Create.Table("Bets")
+            .WithColumn("Id").AsGuid().PrimaryKey().WithDefault(SystemMethods.NewGuid)
+            .WithColumn("Name").AsString(255)
+            .WithColumn("Date").AsDateTime()
+            .WithColumn("BetableEntityA").AsGuid()
+            .WithColumn("BetableEntityB").AsGuid();
+        }
+
         public override void Down()
         {
             Delete.Table("Bets");
-        }
-
-        public override void Up()
-        {
-            if (!Schema.Table("Bets").Exists())
-            {
-                Create.Table("Bets")
-                .WithColumn("Id").AsGuid().PrimaryKey().WithDefault(SystemMethods.NewGuid)
-                .WithColumn("Name").AsString()
-                .WithColumn("Date").AsDateTime().WithDefaultValue(DateTime.Now)
-                .WithColumn("BetableEntityA").AsGuid()
-                .WithColumn("BetableEntityB").AsGuid();
-            }
         }
     }
 }
--- DataAccess/Migrations/PlacedBetCreator.cs
+++ DataAccess/Migrations/PlacedBetCreator.cs
@@ -5,22 +5,19 @@ namespace DataAccess.Migrations
     [Migration(202407157)]
     public class Migration_202407157_Add_PlacedBets : Migration
     {
+        public override void Up()
+        {
+            Create.Table("PlacedBets")
+            .WithColumn("Id").AsGuid().PrimaryKey().WithDefault(SystemMethods.NewGuid)
+            .WithColumn("UserId").AsGuid()
+            .WithColumn("PlacedDate").AsDateTime()
+            .WithColumn("Type").AsInt32()
+            .WithColumn("QuoteId").AsGuid();
+        }
+
         public override void Down()
         {
             Delete.Table("PlacedBets");
-        }
-
-        public override void Up()
-        {
-            if (!Schema.Table("PlacedBets").Exists())
-            {
-                Create.Table("PlacedBets")
-                 .WithColumn("Id").AsGuid().PrimaryKey().WithDefault(SystemMethods.NewGuid)
-                 .WithColumn("UserId ").AsGuid()
-                 .WithColumn("PlacedDate").AsDateTime().WithDefaultValue(DateTime.Now)
-                 .WithColumn("Type").AsString()
-                 .WithColumn("QuoteId").AsGuid();
-            }
         }
     }
 }
