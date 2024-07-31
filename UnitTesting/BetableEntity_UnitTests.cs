9fb9c3bbf370878b0083d9ed4c9edf187a992044
Robert Adam <robert.adam@uipath.com>
2024-07-31 14:31:21 +0300

    Feature: Create tests for get entities

--- UnitTesting/BetableEntity_UnitTests.cs
+++ UnitTesting/BetableEntity_UnitTests.cs
@@ -12,6 +12,7 @@ namespace UnitTesting
         private readonly string ApiUrl = "https://localhost:7291/api";
         static readonly HttpClient client = new HttpClient();
         private BetableEntityDto testObject;
+        private long noEntities = 0;
 
         public BetableEntity_UnitTests(ITestOutputHelper output)
         {
@@ -19,13 +20,27 @@ namespace UnitTesting
         }
 
         [Fact]
-        public async Task Get_All_Entities()
+        public async Task GetAllEntitiesAsync()
         {
-            
+            string getAllUrl = $"{ApiUrl}/BetEntity/all";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(getAllUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                IEnumerable<BetableEntityDto> responseObjects = JsonConvert.DeserializeObject<IEnumerable<BetableEntityDto>>(responseBody);
+                noEntities = responseObjects.LongCount();
+            }
+            catch (Exception ex)
+            {
+                _output.WriteLine($"Error: {ex.Message}");
+            }
+
         }
 
         [Fact]
-        public async Task Post_Entity()
+        public async Task PostEntityAsync()
         {
             CreateBetableEntityDto createBetableEntityDto = new CreateBetableEntityDto { Name = $"BetableEntity {Guid.NewGuid()}" };
             var body = System.Text.Json.JsonSerializer.Serialize(createBetableEntityDto);
@@ -42,8 +57,37 @@ namespace UnitTesting
             }
             catch (Exception ex)
             {
-                _output.WriteLine($"Message: {ex.Message}");
-                Assert.True(false);
+                _output.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        private bool compareEntities(BetableEntityDto entity1, BetableEntityDto entity2)
+        {
+            return entity1.Id == entity2.Id && entity1.Name == entity2.Name;  
+        }
+
+        [Fact]
+        public async Task GetEntityByIdAsync()
+        {
+            try
+            {
+                Guid id = testObject.Id;
+                if (id == Guid.Empty)
+                {
+                    throw new Exception("Test object was not created");
+                }
+
+                string getEntityUrl = $"{ApiUrl}/BetEntity/${id}";
+
+                HttpResponseMessage response = await client.GetAsync(getEntityUrl);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                BetableEntityDto responseObjects = JsonConvert.DeserializeObject<BetableEntityDto>(responseBody);
+                Assert.True(compareEntities(responseObjects, testObject));
+            }
+            catch (Exception ex)
+            {
+                _output.WriteLine($"Error: {ex.Message}");
             }
         }
 
